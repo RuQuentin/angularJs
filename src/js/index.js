52769538168f01cd9582929f6bc9fddab52d9cdf
Kirill Kovzel <Kirill.Kovzel@gmail.com>
2019-02-12 11:10:28 +0300

    features/angularMethods, adds methods list to index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,29 @@
-// code will be here
+(function() {
+  const directives = [];
+
+  const smallAngular = {
+    directive()  {
+
+    },
+
+    compile(node) {
+      // apply direcitves to this node
+    },
+
+    bootstrap(node) {
+
+      node.queryselectorAll('*').forEach(el => {
+        compile(el)
+      })
+    })
+  }
+
+  smallAngular.directive('model', function(el) {
+
+  })
+
+  smallAngular.directive('click', function(el) {
+
+  })
+  
+})()
