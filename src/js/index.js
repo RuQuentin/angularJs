1a69ffca55334c51885e60409ba558d592754150
Kirill Kovzel <Kirill.Kovzel@gmail.com>
2019-02-12 13:50:45 +0300

    features/angularMethods, adds directive method

--- src/js/index.js
+++ src/js/index.js
@@ -1,29 +1,52 @@
 (function() {
-  const directives = [];
-
   const smallAngular = {
-    directive()  {
-
+    directive(name, fn)  {
+      const index = directives.findIndex(el => Object.keys(el)[0] === name);
+  
+      if (index === -1) {
+        directives.push({[name]: [fn]});
+      }
+  
+      if (index !== -1) {
+        directives[index][name].push(fn);
+      }
     },
 
-    compile(node) {
-      // apply direcitves to this node
+    compile(node){
+      directives['ng-click'].forEach(cb => cb(node))
     },
-
     bootstrap(node) {
 
       node.queryselectorAll('*').forEach(el => {
         compile(el)
       })
-    })
+    }
   }
 
-  smallAngular.directive('model', function(el) {
+  smallAngular.directive('ng-model', function(el){
+
+  })
+  smallAngular.directive('ng-click', function(el){
 
   })
 
-  smallAngular.directive('click', function(el) {
+  smallAngular.directive('ng-show', function(el){
+
+  })
+
+  smallAngular.directive('ng-hide', function(el){
 
   })
   
+  window.smallAngular = smallAngular;
 })()
+
+smallAngular.directive('make_short', function(el){
+
+})
+
+
+smallAngular.bootstrap(node) /*
+mount app to this node
+or 
+find data attr ng-app - mount app to this node
